a2f312d1c470098e42955af890efad4184a2a9dd
Nicolas Zanotti <nicolaszanotti@me.com>
2020-03-13 09:13:59 +0100

    Update balance to use number rather than array

--- completed/balance.ts
+++ completed/balance.ts
@@ -14,20 +14,6 @@ The function should return false for the following strings:
 
 */
 
-class Test {
-    public static results = [];
-
-    static it(description: string, assertion: boolean): void {
-        Test.results.push(assertion);
-        console.log('It ' + description + ': ', assertion);
-    }
-
-    static report(): void {
-        const failed = Test.results.filter(x => x === false).length;
-        console.log(failed === 0 ? 'All tests passed.' : failed + ' tests failed.');
-    }
-}
-
 function isBalanced(text: string = '', left: string = '(', right: string = ')'): boolean {
     if (left.length !== 1 || right.length !== 1) throw new RangeError('The left/right characters need to be 1 in length.');
 
@@ -36,69 +22,71 @@ function isBalanced(text: string = '', left: string = '(', right: string = ')'):
 
     if (parens === null || parens.length === 0) return true;
 
-    const stack = [];
-
-    // Add and remove from the stack
+    // Add and remove from the balance
+    let balance = 0;
     for (let index = 0; index < parens.length; index++) {
         if (parens[index] === left) {
-            stack.push(1);
+            balance += 1;
         } else if (parens[index] === right) {
-            if (stack.length !== 0) {
-                stack.pop()
-            } else {
-                stack.push(1)
-            }
+            if (balance !== 0) balance -= 1;
+            else balance += 1;
         }
     }
 
-    return stack.length === 0;
+    return balance === 0;
 }
 
-Test.it(
-    'is balanced if empty',
-    isBalanced('') === true
+const describe = (name, fn) => console.log(`${name}:`) + fn();
+const test = (name, fn) => console.log(`\t• ${name}:`, fn());
+
+describe(
+    'Function isBalanced',
+    () => {
+        test(
+            'It is balanced if empty.',
+            () => isBalanced('') === true
+        );
+
+        test(
+            'It is balanced if no left/right elements in text',
+            () => isBalanced('lorem') === true
+        );
+
+        test(
+            'It shows balanced for a sentence with single set of parenthesis',
+            () => isBalanced('Lorem (ipsum) dolor.') === true
+        );
+
+        test(
+            'It shows balanced for a sentence with multiple parenthesis',
+            () => isBalanced('(if (zero? x) max (/ 1 x))') === true
+        );
+
+        test(
+            'It shows balanced for two sentences with multiple parenthesis',
+            () => isBalanced('I told him (that it’s not (yet) done). (But he wasn’t listening)') === true
+        );
+
+        test(
+            'It is unbalanced for a smiley emoji',
+            () => isBalanced(':-)') === false
+        );
+
+        test(
+            'It is unbalanced for a series of mismatched parenthesis',
+            () => isBalanced('())(') === false
+        );
+
+        test(
+            'It shows balanced for a sentence with single set of custom elements',
+            () => isBalanced('Lorem [ipsum] dolor.', '[', ']') === true
+        );
+
+        try {
+            isBalanced('Lorem [ipsum] dolor.', 'foo', 'bar')
+            test('It throws an error if left/right elements too long', () => false);
+        } catch (e) {
+            test('It throws an error if left/right elements too long', () => true);
+        }
+    }
 );
-
-Test.it(
-    'is balanced if no left/right elements in text',
-    isBalanced('lorem') === true
-);
-
-Test.it(
-    'shows balanced for a sentence with single set of parenthesis',
-    isBalanced('Lorem (ipsum) dolor.') === true
-);
-
-Test.it(
-    'shows balanced for a sentence with multiple parenthesis',
-    isBalanced('(if (zero? x) max (/ 1 x))') === true
-);
-
-Test.it(
-    'shows balanced for two sentences with multiple parenthesis',
-    isBalanced('I told him (that it’s not (yet) done). (But he wasn’t listening)') === true
-);
-
-Test.it(
-    'is unbalanced for a smiley emoji',
-    isBalanced(':-)') === false
-);
-
-Test.it(
-    'is unbalanced for a series of mismatched parenthesis',
-    isBalanced('())(') === false
-);
-
-Test.it(
-    'shows balanced for a sentence with single set of custom elements',
-    isBalanced('Lorem [ipsum] dolor.', '[', ']') === true
-);
-
-try {
-    isBalanced('Lorem [ipsum] dolor.', 'foo', 'bar')
-    Test.it('throws an error if left/right elements too long', false);
-} catch (e) {
-    Test.it('throws an error if left/right elements too long', true);
-}
-
-Test.report();
